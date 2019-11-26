3e2cd28be6588f896a133ea7f02168c1332d5ba9
Eroar <48735008+Eroar@users.noreply.github.com>
2019-11-26 21:39:03 +0100

    Update text2mp3.py

--- text2mp3.py
+++ text2mp3.py
@@ -1,16 +1,23 @@
 from gtts import gTTS
 import sys
+from pydub import AudioSegment
+
+def getTtsFromString(string, lang):
+    '''
+    fileName with no extension
+    '''
+    return gTTS(string, lang)
 
 def main():
     lang = sys.argv[1]
     with open("text2mp3Input.txt", "r") as f:
         s = f.read()
+    
+    tts = getTtsFromString(s)
+    fileName += ".mp3"
+    tts.save(fileName)
 
-    file = "text2mp3Output.mp3"
 
-    # initialize tts, create mp3 and play
-    tts = gTTS(s, lang)
-    tts.save(file)
 
 if __name__ == "__main__":
     main()
